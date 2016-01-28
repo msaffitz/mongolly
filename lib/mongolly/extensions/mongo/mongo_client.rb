2571bfb1e8d25fe897a83da46e6032239deaf04b
Michael Saffitz <m@saffitz.com>
2016-01-27 16:11:36 -0800

    Don't attempt to authenticate without a username

--- lib/mongolly/extensions/mongo/mongo_client.rb
+++ lib/mongolly/extensions/mongo/mongo_client.rb
@@ -195,7 +195,7 @@ protected
 
   def replica_set_connection(hosts, options)
     db = Mongo::MongoReplicaSetClient.new(hosts)
-    db['admin'].authenticate(options[:db_username], options[:db_password])
+    db['admin'].authenticate(options[:db_username], options[:db_password])  if options[:db_username]
     return db
   end
 
